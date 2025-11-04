8aaa46fff3a70c157a029d236b8683e399ba6b20
Logan <34078184+SentientDragon5@users.noreply.github.com>
2025-11-03 21:53:53 -0800

    add waves json

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -1,10 +1,9 @@
 extends Node2D
 
 @onready var enemy = preload("res://Prefabs/enemy.tscn")
-
-
+@onready var enemies: Node2D = $"../Enemies"
 
 func _on_timer_timeout() -> void:
 	var e = enemy.instantiate()
-	e.position = position
-	get_parent().add_child(e)
+	e.global_position = global_position
+	enemies.add_child(e)
